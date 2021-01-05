ab917b80121cb271045263ce0ed112ea2fcd783d
Cristin O'Connor <her@cristin.io>
2021-01-04 21:49:40 -0700

    updated readme

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import { Card } from 'react-github-stats-card-v2';
 function App() {
   return (
     <div className="App">
-      <h1 style={{fontFamily: 'Trebuchet MS', textTransform: 'uppercase', textAlign: 'center'}}>React GitHub Stats Card v2</h1>
       <Card username="cnocon" theme={true} />
     </div>
   );
